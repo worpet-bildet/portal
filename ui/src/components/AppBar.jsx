9773f7104acf2df0c28fbf6ecc4bb8bc2c6b812d
morelazers <contacttomnash@gmail.com>
2023-02-22 16:10:38 +0100

    feat: blurb (hardcoded rn)

--- ui/src/components/AppBar.jsx
+++ ui/src/components/AppBar.jsx
@@ -1,7 +1,9 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { sigil, reactRenderer } from "@tlon/sigil-js";
 import { getSelectedSection, useStore } from "../state/store";
+import { useUrbit, usePortal } from "../state/usePortal";
 
 const navigation = [
   { name: "Home", href: "#", highlightOnSelect: true, section: "all" },
@@ -30,6 +32,28 @@ export default function Example() {
     evt.preventDefault();
     item.highlightOnSelect && setSelectedSection(item.section.toLowerCase());
   };
+
+  const mySigil = () => {
+    // TODO: check this works when signed in on a ship
+    const [myShip, setMyShip] = useState();
+    const { ship } = usePortal();
+    useEffect(() => {
+      if (ship) setMyShip(ship);
+    }, [ship]);
+    // sigil-js can't render moons
+    if (myShip && myShip.length > "13") return <></>;
+    return (
+      <>
+        {sigil({
+          patp: myShip || "zod",
+          renderer: reactRenderer,
+          size: "50",
+          colors: ["black", "white"],
+        })}
+      </>
+    );
+  };
+
   return (
     <Disclosure as="nav" className="bg-black">
       {({ open }) => (
@@ -86,6 +110,7 @@ export default function Example() {
                         {item.name}
                       </a>
                     ))}
+                    <div>{mySigil()}</div>
                   </div>
                 </div>
               </div>
