0c650f2a36252eac9ff644848094e2c18406fb26
morelazers <contacttomnash@gmail.com>
2023-02-23 01:05:16 +0100

    feat: scripts for data

--- ui/src/components/Item/ItemImage.jsx
+++ ui/src/components/Item/ItemImage.jsx
@@ -10,6 +10,7 @@ export const ItemImage = ({ src, type, container, onError }) => {
   }, [container]);
 
   if (type === "ship") {
+    if (src.length > "13") return <></>
     return (
       <>
         {sigil({
