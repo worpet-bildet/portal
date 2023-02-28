9cb84617dc0b7a22c28893d87c6553ec769bd5a7
morelazers <contacttomnash@gmail.com>
2023-02-28 14:50:32 +0100

    fix: dist stuff

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -7,8 +7,7 @@ import { defineConfig, loadEnv } from "vite";
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   Object.assign(process.env, loadEnv(mode, process.cwd()));
-  const SHIP_URL =
-    process.env.SHIP_URL || process.env.VITE_SHIP_URL || "http://localhost:8080";
+  const SHIP_URL = process.env.SHIP_URL || process.env.VITE_SHIP_URL || "";
   console.log(SHIP_URL);
 
   return defineConfig({
