4c4fdf9290699d36d9b764d881e8a1ab81d2681f
morelazers <contacttomnash@gmail.com>
2023-03-31 16:55:07 +0200

    fix: left pad h/m/s

--- ui/src/utils/format.js
+++ ui/src/utils/format.js
@@ -166,7 +166,9 @@ export const unsanitiseTextFieldsRecursive = objectOrText => {
 export const urbitTime = timestamp => {
   // turn this into a date object
   const date = new Date(timestamp);
-  return `~${date.getUTCFullYear()}.${
-    date.getUTCMonth() + 1
-  }.${date.getUTCDate()}..${date.getUTCHours()}.${date.getUTCMinutes()}.${date.getUTCSeconds()}`;
+  return `~${date.getUTCFullYear()}.${date.getUTCMonth() + 1}.${date.getUTCDate()}..${
+    date.getUTCHours() < 10 ? "0" + date.getUTCHours() : date.getUTCHours()
+  }.${date.getUTCMinutes() < 10 ? "0" + date.getUTCMinutes() : date.getUTCMinutes()}.${
+    date.getUTCSeconds() < 10 ? "0" + date.getUTCSeconds() : date.getUTCSeconds()
+  }`;
 };
