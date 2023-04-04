0ff7c0c495b52170aa0a255470e4d3f9180a594c
morelazers <contacttomnash@gmail.com>
2023-04-04 15:40:38 +0200

    chore: refactor

---
+++ ui/src/Layout.jsx
@@ -0,0 +1,32 @@
+import React, { Fragment, useEffect } from "react";
+import ReactGA4 from "react-ga4";
+import NavBar from "./components/NavBar";
+import { Outlet, useMatch, useNavigate, useLocation } from "react-router-dom";
+
+export default function Layout() {
+  const navigate = useNavigate();
+  const atRoot = useMatch("/");
+  useEffect(() => {
+    if (atRoot) navigate("/~worpet-bildet");
+  }, [atRoot]);
+
+  const location = useLocation();
+  useEffect(() => {
+    ReactGA4.send({
+      hitType: "pageview",
+      page: location.pathname,
+      title: location.pathname,
+    });
+  }, [location]);
+
+  return (
+    <Fragment>
+      <NavBar />
+      <div className="px-2 sm:px-5 lg:px-24">
+        <div className="flex justify-center max-w-full min-h-screen">
+          <Outlet />
+        </div>
+      </div>
+    </Fragment>
+  );
+}
