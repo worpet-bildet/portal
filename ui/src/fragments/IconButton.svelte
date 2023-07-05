2399cdc5b3cdc291559de8f4f51c9622597d663b
morelazers <contacttomnash@gmail.com>
2023-07-05 12:54:55 +0200

    fix: cross to close modla

--- ui/src/fragments/IconButton.svelte
+++ ui/src/fragments/IconButton.svelte
@@ -11,7 +11,7 @@
 </script>
 
 <button
-  class="py-2 px-2 flex hover:bg-hover items-center gap-4 hover:duration-500 overflow-hidden rounded-lg dark:fill-white ${$$props.classes}"
+  class="py-2 px-2 flex hover:bg-hover items-center gap-4 hover:duration-500 overflow-hidden rounded-lg dark:fill-white {$$props.classes}"
   class:pointer-events-none={disabled}
   class:opacity-50={disabled}
   class:cursor-not-allowed={disabled}
