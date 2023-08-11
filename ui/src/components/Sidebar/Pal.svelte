847e7391b209ccc2644ffe88f2f9b2e57c45d2a6
morelazers <contacttomnash@gmail.com>
2023-08-11 13:19:38 +0200

    fix: change sigil component to display custom avatar if set

--- ui/src/components/Sidebar/Pal.svelte
+++ ui/src/components/Sidebar/Pal.svelte
@@ -3,13 +3,12 @@
   import { state, getCurator } from '@root/state';
   import { getMeta } from '@root/util';
   import { Sigil } from '@components';
-  import { ItemImage } from '@fragments';
 
   export let pal, score;
 
-  let image, title;
+  let title;
   state.subscribe(() => {
-    ({ title, image } = getMeta(getCurator(pal)));
+    ({ title } = getMeta(getCurator(pal)));
   });
 </script>
 
@@ -20,15 +19,9 @@
     class="flex gap-4 items-center p-1 hover:bg-panels-hover hover:duration-500 rounded-lg dark:border dark:bg-transparent dark:border-transparent dark:hover:border-white"
   >
     <div>
-      {#if image}
-        <div class="rounded-md w-10 h-10 overflow-hidden">
-          <ItemImage {image} title={pal} />
-        </div>
-      {:else}
-        <div class="rounded-md w-10 h-10 overflow-hidden">
-          <Sigil patp={pal} />
-        </div>
-      {/if}
+      <div class="rounded-md w-10 h-10 overflow-hidden">
+        <Sigil patp={pal} />
+      </div>
     </div>
     <div>
       <div class="line-clamp-1">{title || pal}</div>
