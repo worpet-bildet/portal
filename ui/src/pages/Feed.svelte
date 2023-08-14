bae3a2a6e1c0e039792c99dc06a26a8ee4863040
morelazers <contacttomnash@gmail.com>
2023-08-14 16:45:57 +0200

    feat: sort feed posts by ai scor

--- ui/src/pages/Feed.svelte
+++ ui/src/pages/Feed.svelte
@@ -8,6 +8,7 @@
     getCuratorFeed,
     keyStrToObj,
     getCollectedItemLeaderboard,
+    getItem,
   } from '@root/state';
   import {
     Feed,
@@ -51,7 +52,8 @@
       .filter((a, idx) => {
         return mergedFeed.findIndex((b) => b.time === a.time) === idx;
       })
-      .sort((a, b) => fromUrbitTime(b.time) - fromUrbitTime(a.time));
+      .sort((a, b) => getItem(b.key).score - getItem(a.key).score);
+    // .sort((a, b) => fromUrbitTime(b.time) - fromUrbitTime(a.time));
 
     // Get the latest post, if it was more than six hours ago, send another sub
     if (
